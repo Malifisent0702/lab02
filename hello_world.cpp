6bb65ca51b947c55010690c954e6fa5ced22dba6
Malifisent0702 <egorkoshelev07022008@mail.ru>
2026-04-29 14:44:21 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,10 @@
-#include <iostream> // для std::cin, std::cout
-#include <string> // для std::string
+#include <iostream>   // для std::cin, std::cout
+#include <string>   // для std::string
 
 int main() {
-    std::string name; // переменная для хранения имени пользователя
-    std::cout << "Enter your name: "; // запрос имени
-    std::cin >> name; // чтение имени из стандартного ввода
-    std::cout << "Hello world from " << name << std::endl; // приветствие 
+    std::string name;   // переменная для хранения имени пользователя
+    std::cout << "Enter your name: ";   // запрос имени
+    std::cin >> name;   // чтение имени из стандартного ввода
+    std::cout << "Hello world from " << name << std::endl;   // приветствие 
     return 0;
 }
